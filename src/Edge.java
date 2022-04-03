bc2c24d26acd3a963abe8f2723da0b77b2bf6c6d
mrozo <moreez@protonmail.com>
2022-04-03 22:54:33 +0100

    vertex and edge classes updated

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Edge {
+public class Edge {
+    public Vertex from;
+    public Vertex to;
+    public double distance;
+    public int tripId;
+
+    Edge (Vertex from, Vertex to, double distance, int tripId) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+        this.tripId = tripId;
+    }
+
 }
